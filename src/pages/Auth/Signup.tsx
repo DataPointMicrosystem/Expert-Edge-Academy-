8686e4c4ff4f4dadfaaad84e863164b9a18d43fb
DevEvans-tech <uchechukwuevans3@gmail.com>
2026-08-18 16:44:22 -0700

    working on the Landingpage

--- src/pages/Auth/Signup.tsx
+++ src/pages/Auth/Signup.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../../context/AuthContext";
 
 export default function Signup() {
   const { login } = useAuth();
